bef0f71ac33e2961c6e13f036aa8098a8e7b205f
jackarmley <jack_armley@yahoo.co.uk>
2015-07-20 08:34:21 +0100

    added gulp

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+/**
+ * Some sweet gulp action,
+ * 'cause who doesn't like burgers
+ * and shakes together, yeah?
+ */
+
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
